febf2a196e5a199ddb472f75dbba2e4abbff1ee0
hitomi numari <h.h.hitomi1216@gmail.com>
2020-05-15 23:52:37 +0900

    ページネーション設定

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,8 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
 
+  PER = 5
+
   def index
     @search_params = task_search_params
     @tasks = Task.search(@search_params)
@@ -11,6 +13,9 @@ class TasksController < ApplicationController
     else
       @tasks = Task.all.order(created_at:"DESC")
     end
+
+    @tasks = @tasks.page(params[:page]).per(PER)
+
   end
 
   def new
