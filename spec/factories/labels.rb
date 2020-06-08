405628ed512ae23b59043748d9e97826e030a429
hitomi numari <h.h.hitomi1216@gmail.com>
2020-06-09 01:29:38 +0900

    ラベル登録テスト不可

--- spec/factories/labels.rb
+++ spec/factories/labels.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :label do
-    name { "MyString" }
+    name { "Factoryで作ったデフォルトのラベル１" }
+  end
+  factory :second_label, class: Label do
+    name { "Factoryで作ったデフォルトのラベル２" }
+  end
+  factory :third_label, class: Label do
+    name { "Factoryで作ったデフォルトのラベル３" }
   end
 end
--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -6,6 +6,12 @@ RSpec.describe 'タスク管理機能', type: :system do
     @task1 = create(:task, user: @user1)
     @task2 = create(:second_task, user: @user1)
     @task3 = create(:third_task, user: @user1)
+    @label1 = create(:label)
+    @label2 = create(:second_label)
+    @label3 = create(:third_label)
+    create(:labeling, task: @task1, label: @label1)
+    create(:labeling, task: @task1, label: @label2)
+    create(:labeling, task: @task1, label: @label3)
     visit root_path
     fill_in 'session_email', with: 'test_user_01@sample.com'
     fill_in "session_password", with: '00000000'
@@ -59,7 +65,21 @@ RSpec.describe 'タスク管理機能', type: :system do
         select "未着手", from: "search_status"
         click_on '検索'
         expect(page).to have_content 'Factoryで作ったデフォルトのタイトル３', "未着手"
-        expect(page).to have_content '未着手'
+      end
+      it "scopeメソッドでラベル検索ができる" do
+        visit tasks_path
+        select "Factoryで作ったデフォルトのラベル２", from: "search_label_id"
+        click_on '検索'
+        expect(page).to have_content 'Factoryで作ったデフォルトのラベル２'
+      end
+      it "scopeメソッドでタイトル、ステータス、ラベル検索ができる" do
+        visit tasks_path
+        fill_in 'search_title', with: "Factoryで作ったデフォルトのタイトル１"
+        select "着手中", from: "search_status"
+        select "Factoryで作ったデフォルトのラベル１", from: "search_label_id"
+        click_on '検索'
+        expect(page).to have_content 'Factoryで作ったデフォルトのタイトル１', "着手中"
+        expect(page).to have_content "Factoryで作ったデフォルトのラベル１"
       end
     end
   end
@@ -70,13 +90,21 @@ RSpec.describe 'タスク管理機能', type: :system do
         visit new_task_path
         fill_in '名前', with: '会議の準備'
         fill_in '詳細', with: '資料の作成'
+        check "Factoryで作ったデフォルトのラベル１"
+        check "Factoryで作ったデフォルトのラベル３"
         select '2020', from: 'task_deadline_1i'
         select '10', from: 'task_deadline_2i'
         select '5', from: 'task_deadline_3i'
         select '未着手', from: 'task_status'
         select '高', from: 'task_priority'
         click_on "登録"
+        expect(page).to have_content '会議の準備'
         expect(page).to have_content '資料の作成'
+        expect(page).to have_content "Factoryで作ったデフォルトのラベル１"
+        expect(page).to have_content 'Factoryで作ったデフォルトのラベル３'
+        expect(page).to have_content '2020/10/5'
+        expect(page).to have_content '未着手'
+        expect(page).to have_content '高'
       end
     end
   end
